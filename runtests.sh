1cd1d189de05bbd7380d8b38474e6eb94bbf83cb
Samuel Colvin <s@muelcolvin.com>
2015-05-25 13:32:56 +0100

    add coverage

--- runtests.sh
+++ runtests.sh
@@ -5,7 +5,8 @@ if [ "$#" -eq 1 ]; then
 else
   test_path="django_websockets"
 fi
+django_admin=$(which django-admin.py)
 export ASYNC_TEST_TIMEOUT="1"
 export DJANGO_SETTINGS_MODULE="django_websockets.testsettings"
 export PYTHONPATH=.
-django-admin.py test ${test_path}
+time coverage run --source=django_websockets $django_admin test $test_path
